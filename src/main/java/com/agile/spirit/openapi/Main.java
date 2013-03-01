69f1aa5cf578663b2866da035453c04d54c091c7
Jérémy Buget <jbuget@octo.com>
2013-03-01 11:10:46 +0100

    Manage Port property as a System variable

--- src/main/java/com/agile/spirit/openapi/Main.java
+++ src/main/java/com/agile/spirit/openapi/Main.java
@@ -9,7 +9,6 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
-import com.agile.spirit.openapi.utils.PersistenceUtil;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
@@ -25,41 +24,63 @@ public class Main {
         }
     }
 
-    private static int getPort(int defaultPort) {
-        String port = System.getProperty("jersey.test.port");
-        if (null != port) {
-            try {
-                return Integer.parseInt(port);
-            } catch (NumberFormatException e) {
-            }
-        }
-        return defaultPort;
+    // private static int getPort(int defaultPort) {
+    // String port = System.getProperty("jersey.test.port");
+    // if (null != port) {
+    // try {
+    // return Integer.parseInt(port);
+    // } catch (NumberFormatException e) {
+    // }
+    // }
+    // return defaultPort;
+    // }
+    //
+    // private static URI getBaseURI() {
+    // return
+    // UriBuilder.fromUri("http://openapi.herokuapp.com/").port(getPort(9998)).build();
+    // }
+    //
+    // public static final URI BASE_URI = getBaseURI();
+    //
+    // protected static HttpServer startServer() throws IOException {
+    // System.out.println("Starting grizzly...");
+    // ResourceConfig rc = new
+    // PackagesResourceConfig("com.agile.spirit.openapi");
+    // return GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
+    // }
+    //
+    // public static void main(String[] args) throws IOException {
+    // PersistenceUtil.createEntityManagerFactory();
+    // em = PersistenceUtil.getEntityManager();
+    //
+    // addNotes();
+    //
+    // HttpServer httpServer = startServer();
+    // System.out.println(String.format("Jersey app started with WADL available at "
+    // + "%sapplication.wadl\nHit enter to stop it...", BASE_URI, BASE_URI));
+    // System.in.read();
+    // httpServer.stop();
+    //
+    // PersistenceUtil.closeEntityManagerFactory();
+    // }
+
+    private static URI getBaseURI(String hostname, int port) {
+        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
     }
 
-    private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://openapi.herokuapp.com/").port(getPort(9998)).build();
-    }
-
-    public static final URI BASE_URI = getBaseURI();
-
-    protected static HttpServer startServer() throws IOException {
+    protected static HttpServer startServer(URI uri) throws IOException {
         System.out.println("Starting grizzly...");
         ResourceConfig rc = new PackagesResourceConfig("com.agile.spirit.openapi");
-        return GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
+        return GrizzlyServerFactory.createHttpServer(uri, rc);
     }
 
     public static void main(String[] args) throws IOException {
-        PersistenceUtil.createEntityManagerFactory();
-        em = PersistenceUtil.getEntityManager();
-
-        addNotes();
-
-        HttpServer httpServer = startServer();
+        URI uri = getBaseURI(System.getenv("HOSTNAME"), Integer.valueOf(System.getenv("PORT")));
+        HttpServer httpServer = startServer(uri);
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI, BASE_URI));
-        System.in.read();
-        httpServer.stop();
-
-        PersistenceUtil.closeEntityManagerFactory();
+                + "%sapplication.wadl\nHit enter to stop it...", uri, uri));
+        while (true) {
+            System.in.read();
+        }
     }
 }
